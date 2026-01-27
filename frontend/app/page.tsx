6b42fd9ededfbbf2b59e4d2ce73c180c91ba56fb
Oshioke Salaki <salaki1902@gmail.com>
2026-01-27 21:14:43 +0100

    connect wallet (#40)
    * connect wallet
    
    * fix: build

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,10 +8,12 @@ import TestimonialsSection from "./landing/components/TestimonialsSection";
 import StatsSection from "./landing/components/StatsSection";
 import FaqSection from "./landing/components/FaqSection";
 import Footer from "./landing/components/Footer";
+import { Navbar } from "@/components/Navbar";
 
 export default function Page() {
   return (
     <main className="min-h-screen w-full text-white overflow-x-hidden">
+      <Navbar />
       <HeroSection />
       <div className="py-25">
         <IntroSection />
